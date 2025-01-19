5d3c7bd0aef6190547df5562b612a5f61203be59
exxardo <eduardoramirodematos@gmail.com>
2025-01-19 19:36:25 -0300

    Initial commit

---
+++ src/main/java/com/carservice/storecarjava11/StoreCarJava11Application.java
@@ -0,0 +1,13 @@
+package com.carservice.storecarjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCarJava11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCarJava11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/carservice/storecarjava11/StoreCarJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carservice.storecarjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCarJava11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
